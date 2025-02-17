75c69d140b379029febf95604e02c0754d7d4b4a
jasbo-777 <jasminbains7@gmail.com>
2025-02-17 13:27:51 -0800

    Add files via upload
    MyStuff - package mybook

---
+++ MyStuff.java
@@ -0,0 +1,15 @@
+// MyStuff
+//JB
+
+public class MyStuff {
+    public static void main(String[] args) {
+
+        Book myBook = new Book();
+        myBook.title = "A Court of Thorns and Roses";
+        myBook.author = "Sarah J Maas";
+        myBook.ISBN = "9781635575569";
+        myBook.description = "A Court of Thorns and Roses is a fantasy romance series";
+
+        System.out.println(myBook.author);
+        }
+    }
