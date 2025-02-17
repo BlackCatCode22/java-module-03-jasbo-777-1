df3414e9d1684f5175157a60e10e5e6f76872a0d
jasbo-777 <jasminbains7@gmail.com>
2025-02-17 13:15:37 -0800

    Add files via upload
    MyStudent package

---
+++ MyStudent.java
@@ -0,0 +1,26 @@
+// MyStudent
+//JB
+
+public class MyStudent {
+    public static void main(String[] args) {
+
+        Student myStudent = new Student();
+        myStudent.firstName = "Jim";
+        myStudent.lastName = "Halpert";
+        myStudent.major = "Business";
+        myStudent.gpa = 2.3;
+        myStudent.age = 24;
+        myStudent.onProbation = false;
+
+        Student myStudent2 = new Student();
+        myStudent.firstName = "Pam";
+        myStudent.lastName = "Beasley";
+        myStudent.major = "Art";
+        myStudent.gpa = 2.5;
+        myStudent.age = 23;
+        myStudent.onProbation = true;
+
+        System.out.println(myStudent2.firstName);
+
+        }
+    }
