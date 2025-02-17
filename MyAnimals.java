b4a89d39a5cbecdf72874f2b822968ac72d9bc79
jasbo-777 <jasminbains7@gmail.com>
2025-02-17 13:43:58 -0800

    Add files via upload

---
+++ MyAnimals.java
@@ -0,0 +1,17 @@
+// MyAnimals
+// JB
+
+public class MyAnimals {
+    public static void main(String[] args) {
+        System.out.println(Cat.getCatCount());
+
+        Cat myCat = new Cat();
+        myCat.meow();
+        myCat.name = "Stella";
+        myCat.age = 8;
+
+        System.out.println(Cat.MAX_LIVES);
+        System.out.println(Cat.getCatCount());
+
+        }
+    }
